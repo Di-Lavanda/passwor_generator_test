4845e3cc63766d1af157cefb91d7feeb023376af
Di-Lavanda <50248066+Di-Lavanda@users.noreply.github.com>
2021-09-27 15:52:17 +0400

    first commit

---
+++ password_generator_test/password_generator_test/Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace password_generator_test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxLenght = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.specSymboltextBox = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(37, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Длина";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(142, 64);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(15, 14);
+            this.checkBox1.TabIndex = 1;
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(142, 87);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(15, 14);
+            this.checkBox2.TabIndex = 2;
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(37, 61);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Цифры";
+            // 
+            // textBoxLenght
+            // 
+            this.textBoxLenght.Location = new System.Drawing.Point(141, 38);
+            this.textBoxLenght.Name = "textBoxLenght";
+            this.textBoxLenght.Size = new System.Drawing.Size(100, 20);
+            this.textBoxLenght.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(37, 87);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(97, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Заглавные буквы";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(37, 107);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(101, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Знаки препинания";
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(142, 109);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(15, 14);
+            this.checkBox3.TabIndex = 7;
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(37, 131);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(81, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Спец символы";
+            // 
+            // specSymboltextBox
+            // 
+            this.specSymboltextBox.Location = new System.Drawing.Point(140, 132);
+            this.specSymboltextBox.Name = "specSymboltextBox";
+            this.specSymboltextBox.Size = new System.Drawing.Size(100, 20);
+            this.specSymboltextBox.TabIndex = 9;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(322, 176);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 10;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(322, 202);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 23);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Generate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.specSymboltextBox);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxLenght);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxLenght;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox specSymboltextBox;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ password_generator_test/password_generator_test/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace password_generator_test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+
+        private void button1_Click(object sender, EventArgs e) //основная функция в программе, можно вынести в отдельный блок, но пока работает через кнопку
+        {
+            Random rnd = new Random();
+            
+            string undercaseletters = "abcdefghijklmnopqrstuvwxyz"; //пароль не может быть пустым поэтому буквы нижнего регистра используются как основа
+            char tempstr;   //временная переменная
+            string result = ""; //результат
+            int lenght = 0;     //длина пароля
+            if (string.IsNullOrEmpty(textBoxLenght.Text))   //проверка не пустое ли поле с занчением длины
+                MessageBox.Show("Введите длину");
+            else
+            {
+                lenght = Convert.ToInt32(textBoxLenght.Text);
+                for (int i = 1; i <= lenght; i++)   //основной цикл функции
+                {
+                    int a = rnd.Next(0, 25);
+                    tempstr = undercaseletters[a];
+                    result += tempstr;
+                    if ((checkBox1.Checked) && (i < lenght)) //вторая проверка необходима для паролей нечетной длины
+                    {
+                        a = rnd.Next(0, 9);
+                        result += numbersFunction(a);   //включает цифры в пароль
+                        i++;
+                    }
+                    if ((checkBox2.Checked) && (i < lenght))
+                    {
+                        a = rnd.Next(0, 25);
+                        result += uppercaseFunction(a); //включает заглавные буквы в пароль
+                        i++;
+                    }
+                    if ((checkBox3.Checked) && (i < lenght))
+                    {
+                        a = rnd.Next(0, 9);
+                        result += punctuationFunction(a);   //вкулючает знаки пукнтуации в пароль
+                        i++;
+                    }
+                    if (!(string.IsNullOrEmpty(specSymboltextBox.Text)))
+                    {
+                        int lenghtSpecSymbol = (specSymboltextBox.Text).Length;
+                        a = rnd.Next(0, lenghtSpecSymbol);
+                        result += specSymbolFunction(a);    //включает спецсимволы в пароль
+                        i++;
+                    }
+                }
+                textBox3.Text = result;
+            }
+        }       //фнукции со словарями
+        public char uppercaseFunction(int a)
+        {           
+            string uppercaseletters = "ABCDEFGHIJKLMNOPQRSTUVQXYZ";
+            return uppercaseletters[a];
+        }
+        public char numbersFunction(int a)
+        {
+            string uppercaseletters = "0123456789";
+            return uppercaseletters[a];
+        }
+        public char punctuationFunction(int a)
+        {
+            string uppercaseletters = ".,?!;:-()'";
+            return uppercaseletters[a];
+        }
+        public char specSymbolFunction(int a)
+        {
+            string specSymbol = (specSymboltextBox.Text);
+            return specSymbol[a];
+        }
+    }
+}
